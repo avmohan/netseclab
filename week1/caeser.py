ebf2e07dcfe00953db5cf8c8b8af9de93612583c
Abhijith V Mohan <abhijith.vmohan@gmail.com>
2014-01-09 20:04:27 +0530

    Indent using spaces. no tabs

--- week1/caeser.py
+++ week1/caeser.py
@@ -5,39 +5,39 @@ import frequencygraph
 letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def encrypt(plaintext, key):
-	ciphertext=[]
-	for character in plaintext.upper():
-		if character in letters:
-			ciphertext.append(letters[(letters.index(character)+key)%26])
-		else:
-			ciphertext.append(character)
-	return ''.join(ciphertext)
+    ciphertext=[]
+    for character in plaintext.upper():
+        if character in letters:
+            ciphertext.append(letters[(letters.index(character)+key)%26])
+        else:
+            ciphertext.append(character)
+    return ''.join(ciphertext)
 
 def decrypt(ciphertext, key):
-	plaintext=[]
-	for character in ciphertext.upper():
-		if character in letters:
-			plaintext.append(letters[(letters.index(character)-key)%26])
-		else:
-			plaintext.append(character)
-	return ''.join(plaintext).lower()
+    plaintext=[]
+    for character in ciphertext.upper():
+        if character in letters:
+            plaintext.append(letters[(letters.index(character)-key)%26])
+        else:
+            plaintext.append(character)
+    return ''.join(plaintext).lower()
 
 def main():
-	mode, key, inputfile, outputfile = sys.argv[1:5]
-	key = int(key)
-	ifile = open(inputfile, 'r')
-	ofile = open(outputfile, 'w')
-	if mode=='e':
-		plaintext = ifile.read()
-		ciphertext = encrypt(plaintext, key) 
-		ofile.write(ciphertext)
-		frequencygraph.freqbars_percentage('Caeser Cipher frequency graph (key={0})'.format(key), plaintext, ciphertext)
-	elif mode=='d':
-		ciphertext = ifile.read()
-		plaintext = decrypt(ciphertext, key)
-		ofile.write(plaintext)
-	ifile.close()
-	ofile.close()
+    mode, key, inputfile, outputfile = sys.argv[1:5]
+    key = int(key)
+    ifile = open(inputfile, 'r')
+    ofile = open(outputfile, 'w')
+    if mode=='e':
+        plaintext = ifile.read()
+        ciphertext = encrypt(plaintext, key) 
+        ofile.write(ciphertext)
+        frequencygraph.freqbars_percentage('Caeser Cipher frequency graph (key={0})'.format(key), plaintext, ciphertext)
+    elif mode=='d':
+        ciphertext = ifile.read()
+        plaintext = decrypt(ciphertext, key)
+        ofile.write(plaintext)
+    ifile.close()
+    ofile.close()
 
 if __name__ == '__main__':
-	main()
+    main()
--- week1/vigenere.py
+++ week1/vigenere.py
@@ -5,45 +5,45 @@ import frequencygraph
 letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def encrypt(plaintext, key):
-	print 'encrypt'
-	ciphertext = []
-	inx = 0
-	for character in plaintext.upper():
-		if character in letters:
-			ciphertext.append(letters[(letters.index(character) + letters.index(key[inx]))%26])
-			inx = (inx+1) % len(key)
-		else:
-			ciphertext.append(character)
-	return ''.join(ciphertext)
+    print 'encrypt'
+    ciphertext = []
+    inx = 0
+    for character in plaintext.upper():
+        if character in letters:
+            ciphertext.append(letters[(letters.index(character) + letters.index(key[inx]))%26])
+            inx = (inx+1) % len(key)
+        else:
+            ciphertext.append(character)
+    return ''.join(ciphertext)
 
 def decrypt(ciphertext, key):
-	print 'decrypt'
-	plaintext = []
-	inx = 0
-	for character in ciphertext.upper():
-		if character in letters:
-			plaintext.append(letters[(letters.index(character) - letters.index(key[inx]))%26])
-			inx = (inx+1) % len(key)
-		else:
-			plaintext.append(character)
-	return ''.join(plaintext).lower()
+    print 'decrypt'
+    plaintext = []
+    inx = 0
+    for character in ciphertext.upper():
+        if character in letters:
+            plaintext.append(letters[(letters.index(character) - letters.index(key[inx]))%26])
+            inx = (inx+1) % len(key)
+        else:
+            plaintext.append(character)
+    return ''.join(plaintext).lower()
 
 def main():
-	mode, key, inputfile, outputfile = sys.argv[1:5]
-	key = key.upper()
-	ifile = open(inputfile, 'r')
-	ofile = open(outputfile, 'w')
-	if mode=='e':
-		plaintext = ifile.read()
-		ciphertext = encrypt(plaintext, key) 
-		ofile.write(ciphertext)
-		frequencygraph.freqbars_percentage('Vigenere Cipher frequency graph (key={0})'.format(key), plaintext, ciphertext)
-	elif mode=='d':
-		ciphertext = ifile.read()
-		plaintext = decrypt(ciphertext, key)
-		ofile.write(plaintext)
-	ifile.close()
-	ofile.close()
+    mode, key, inputfile, outputfile = sys.argv[1:5]
+    key = key.upper()
+    ifile = open(inputfile, 'r')
+    ofile = open(outputfile, 'w')
+    if mode=='e':
+        plaintext = ifile.read()
+        ciphertext = encrypt(plaintext, key) 
+        ofile.write(ciphertext)
+        frequencygraph.freqbars_percentage('Vigenere Cipher frequency graph (key={0})'.format(key), plaintext, ciphertext)
+    elif mode=='d':
+        ciphertext = ifile.read()
+        plaintext = decrypt(ciphertext, key)
+        ofile.write(plaintext)
+    ifile.close()
+    ofile.close()
 
 if __name__ == '__main__':
-	main()
+    main()
