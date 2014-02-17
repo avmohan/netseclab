506b5d39198ee25c47b2bddefd16645af7696bc8
Abhijith V Mohan <abhijith.vmohan@gmail.com>
2014-02-17 16:18:38 +0530

    Implemented Linear Congruential Generator

---
+++ week5-prng/lcg.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import sys
+from itertools import islice
+
+def lcg(x0, m=2147483647, a=16807, c=0):
+	while True:
+		x0 = (a*x0 + c) % m
+		yield x0
+
+def main():
+	if len(sys.argv) == 3:
+		x0, limit = [int(x) for x in sys.argv[1:]]
+		print '\n'.join([str(i) for i in islice(lcg(x0), limit)])
+
+	elif len(sys.argv) == 6:
+		x0, m, a, c, limit = [int(x) for x in sys.argv[1:]]
+		print '\n'.join([str(i) for i in islice(lcg(x0, m, a, c), limit)])
+
+	else:
+		print 'Incorrect usage'
+
+if __name__ == '__main__':
+	main()
